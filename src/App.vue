608d144cfd51e1dda89227ef23f5a91721624ea8
cleverdev727 <cleverdev727@gmail.com>
2023-04-27 20:20:09 +0300

    remove space in desktop cricket list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="font-sans" :class="{'dark': theme == 'dark'}">
     <Header @switchTheme="switchTheme()" />
-    <div class="min-h-[calc(100vh-38px)] md:min-h-[calc(100vh-65px)] px-[2px] md:pl-24 md:pr-10 py-[10px] md:pt-5 md:pb-10 dark:bg-[#1E1E1E]">
+    <div class="min-h-[calc(100vh-38px)] md:min-h-[calc(100vh-65px)] px-[2px] md:px-3 py-[10px] md:pt-5 md:pb-10 dark:bg-[#1E1E1E]">
       <MobileTab class="flex md:hidden" />
       <Tab class="hidden md:flex" />
       <router-view />
